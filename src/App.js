7e641d091f268a7421ce05209ecd451f14de68e4
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 18:45:34 +0100

    Added some more components for layouts sake.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 import HotelList from "./components/HotelList";
+import Filter from "./components/Filter";
+import Header from "./components/Header";
+import HomePageBanner from "./components/HomePageBanner";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <HomePageBanner />
+      <Filter />
       <HotelList />
     </div>
   );
