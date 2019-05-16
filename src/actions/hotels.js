1be5d7e862b25a9f9ab5a707008b12dcdf4841e5
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 17:05:27 +0100

    Actually added axios this time 👀

--- src/actions/hotels.js
+++ src/actions/hotels.js
@@ -1,8 +1,11 @@
+import axios from "axios";
 export const setHotels = hotels => ({
   type: "SET_HOTELS",
   hotels
 });
 
 export const startSetHotels = () => {
-  
+  return dispatch => {
+    return axios.get("data.json").then(res => console.log(res));
+  };
 };
