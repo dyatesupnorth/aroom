053112f29185a80e917d2c545ca54bb6a132d214
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 18:36:30 +0100

    updated HotelList test. Updated HotelListItem

--- src/components/HotelListItem.js
+++ src/components/HotelListItem.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
-export class HotelListItem extends Component {
-  render() {
-    return (
-      <div style={{ border: "1px solid tomato" }}>
-        <h1>HotelListItem</h1>
-      </div>
-    );
-  }
-}
+export const HotelListItem = props => {
+  return (
+    <div style={{ border: "1px solid tomato" }}>
+      <h2>{props.name}</h2>
+      <p>Star rating: {props.starRating}</p>
+      {props.facilities.length === 0 ? (
+        <p>No facilities</p>
+      ) : (
+        <p>
+          Facilities:
+          {props.facilities.map(facility => {
+            return <span>{facility}</span>;
+          })}
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default HotelListItem;
--- src/tests/components/HotelList.test.js
+++ src/tests/components/HotelList.test.js
@@ -3,7 +3,12 @@ import { shallow } from "enzyme";
 import { HotelList } from "../../components/HotelList";
 import hotels from "../fixtures/hotels";
 
-test("should render HotelList", () => {
+test("should render HotelList correctly", () => {
   const wrapper = shallow(<HotelList hotels={hotels} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should render HotelList with no hotels correctly", () => {
+  const wrapper = shallow(<HotelList hotels={[]} />);
   expect(wrapper).toMatchSnapshot();
 });
