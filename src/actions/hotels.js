77b60e4f2bc0b7c1ab43eaaa20e23bf8b6b0287b
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 17:23:18 +0100

    Revert "Added node server for test data"
    This reverts commit 821081139dc87e0b4f21dce2a6856bed287a1c2a.

--- src/actions/hotels.js
+++ src/actions/hotels.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3001/'
 export const setHotels = hotels => ({
   type: "SET_HOTELS",
   hotels
@@ -7,8 +6,6 @@ export const setHotels = hotels => ({
 
 export const startSetHotels = () => {
   return dispatch => {
-    return axios.get("hotels").then(res => 
-      dispatch(setHotels(res.data))
-      );
+    return axios.get("data.json").then(res => console.log(res));
   };
 };
