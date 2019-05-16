5b6156ad8c81c88977c97ce40f9be46ccf960f7b
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 18:39:01 +0100

    Added unit test to check for HotelListItem with no facilities

--- src/tests/components/HotelListItem.test.js
+++ src/tests/components/HotelListItem.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { HotelListItem } from "../../components/HotelListItem";
+import hotels from "../fixtures/hotels";
 
-test("should render HotelListItem", () => {
-  const wrapper = shallow(<HotelListItem />);
+test("should render HotelListItem with hotel", () => {
+  const wrapper = shallow(<HotelListItem {...hotels[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+
+test("should render HotelListItem with hotel and no facilities", () => {
+  const wrapper = shallow(<HotelListItem {...hotels[3]} />);
   expect(wrapper).toMatchSnapshot();
 });
