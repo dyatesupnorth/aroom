d6f236eb971e44cad0063203da9306b05af5a6a7
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 16:30:25 +0100

    Added HomePageBanner component. Initial tests passing.

---
+++ src/components/HomePageBanner.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class HomePageBanner extends Component {
+  render() {
+    return (
+      <div style={{ border: "1px solid tomato" }}>
+        <h1>HomePageBanner</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePageBanner;
