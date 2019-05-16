dbffb3f914587125f76597da6aad49a7a68dfe0f
Dave Yates <daveyates@daves-mbp.home>
2019-05-16 16:21:10 +0100

    Added HotelListItem component

---
+++ src/components/HotelListItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class HotelListItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HotelListItem</h1>
+      </div>
+    );
+  }
+}
+
+export default HotelListItem;
