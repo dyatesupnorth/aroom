6cf94768900fc6e839a6fb6f31d3895597d1d049
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:15:33 +0100

    Added test for starRating filter

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -18,3 +18,23 @@ test("should set text filter with params ", () => {
     text
   });
 });
+
+test("should set star rating without params ", () => {
+  const starRating = "";
+  const action = setStarRatingFilter(starRating);
+
+  expect(action).toEqual({
+    type: "SET_STAR_RATING_FILTER",
+    starRating: ""
+  });
+});
+
+test("should set star rating with params", () => {
+  const starRating = 5;
+  const action = setStarRatingFilter(starRating);
+
+  expect(action).toEqual({
+    type: "SET_STAR_RATING_FILTER",
+    starRating
+  });
+});
