2573a32ed9bc649321e963cef787f0ba5144724d
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 17:15:41 +0100

    Updates

---
+++ precache-manifest.bfe41957b37c98979a9176647b49d135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a67b95729489636fd903047ba3a0fa",
+    "url": "/aroom/index.html"
+  },
+  {
+    "revision": "3bace1b821d17d5f72ac",
+    "url": "/aroom/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8aff270b7a5d3a48b615",
+    "url": "/aroom/static/css/main.515283e2.chunk.css"
+  },
+  {
+    "revision": "3bace1b821d17d5f72ac",
+    "url": "/aroom/static/js/2.c2905bf6.chunk.js"
+  },
+  {
+    "revision": "8aff270b7a5d3a48b615",
+    "url": "/aroom/static/js/main.7e07f852.chunk.js"
+  },
+  {
+    "revision": "89ab78a50cfeb5e74371",
+    "url": "/aroom/static/js/runtime~main.978327f8.js"
+  },
+  {
+    "revision": "090b2dc9c935120ad19377657cdf1e9c",
+    "url": "/aroom/static/media/logo.090b2dc9.png"
+  }
+]);
