7d9f805eba28c3e40eeafa090e1a40ae17322a6d
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:46:50 +0100

    Added name filter to Filter component. Hooked it up with connect(). Added filtersReducer to store.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setTextFilter } from "../actions/filters";
 
 export class Filter extends Component {
+  // Handy snippet to handle most cases of form input state changes
+  onTextChange = e => {
+    this.props.setTextFilter(e.target.value);
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <div style={{ border: "1px solid tomato" }}>
         <h1>Filter</h1>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            className="form-control"
+            name="text"
+            type="text"
+            value={this.props.filters.text}
+            onChange={this.onTextChange}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Filter
+const mapStateToProps = state => {
+  console.log("​state", state);
+  return {
+    filters: state.filters
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    setTextFilter: text => dispatch(setTextFilter(text))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Filter);
