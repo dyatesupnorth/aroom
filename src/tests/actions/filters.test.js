13d664f167a211f072475d2712e8b8347c6fd081
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:10:55 +0100

    Added text filter test

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,19 @@
+test('should set text filter without params', () => {
+    const action = setTextFilter();
+  
+    expect(action).toEqual({
+      type: 'SET_TEXT_FILTER',
+      text: ''
+    })
+  })
+  
+  
+  test('should set text filter with params ', () => {
+      const text = 'hotelone'
+      const action = setTextFilter(text);
+  
+      expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text
+      })
+  })
