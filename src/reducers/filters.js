81df0bdfcefc66062f533ad8fb32829932563350
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:28:37 +0100

    SET_TEXT_FILTER reducer implemented

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -5,6 +5,11 @@ const filtersReducerDefaultState = {
 };
 export default (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        text: action.text
+      };
     default:
       return state;
   }
