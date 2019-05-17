786bf7527a0f545760df0cf75413462719716629
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:24:01 +0100

    Added initial test for filtersReducer

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,9 @@
+test("should setup default filter values", () => {
+  // type: @@INIT is a redux reducers initial state, setting it here allows us to test the reducer without the suite blowing up.
+  const state = filtersReducer(undefined, { type: "@@INIT" });
+  expect(state).toEqual({
+    text: "",
+    starRating: "",
+    facilities: []
+  });
+});
