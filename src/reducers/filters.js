836d68f50e062d93ee03c3c1d44c4ef55182dc70
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:26:44 +0100

    Setup filters reducer

---
+++ src/reducers/filters.js
@@ -0,0 +1,11 @@
+const filtersReducerDefaultState = {
+  text: "",
+  starRating: "",
+  facilities: []
+};
+export default (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,3 +1,5 @@
+import filtersReducer from "../../reducers/filters";
+
 test("should setup default filter values", () => {
   // type: @@INIT is a redux reducers initial state, setting it here allows us to test the reducer without the suite blowing up.
   const state = filtersReducer(undefined, { type: "@@INIT" });
