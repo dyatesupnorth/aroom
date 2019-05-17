48787e9e0e1a85f97677de3d1b7a22d7b6c49719
Dave Yates <daveyates@daves-mbp.home>
2019-05-17 09:13:41 +0100

    Added textFilter action

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,19 +1,20 @@
-test('should set text filter without params', () => {
-    const action = setTextFilter();
-  
-    expect(action).toEqual({
-      type: 'SET_TEXT_FILTER',
-      text: ''
-    })
-  })
-  
-  
-  test('should set text filter with params ', () => {
-      const text = 'hotelone'
-      const action = setTextFilter(text);
-  
-      expect(action).toEqual({
-        type: 'SET_TEXT_FILTER',
-        text
-      })
-  })
+import { setTextFilter } from "../../actions/filters";
+
+test("should set text filter without params", () => {
+  const action = setTextFilter();
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
+  });
+});
+
+test("should set text filter with params ", () => {
+  const text = "hotelone";
+  const action = setTextFilter(text);
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text
+  });
+});
