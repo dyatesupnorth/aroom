3fcdfbe0bcf5a28cd74b5e89d3d3b5e831e344ea
Dave Yates <daveyates@daves-mbp.home>
2019-05-20 10:46:35 +0100

    Added fix for hotels request to work locally

--- src/actions/hotels.js
+++ src/actions/hotels.js
@@ -1,13 +1,33 @@
 import axios from "axios";
+
+export const hotels = () => [
+  {
+    name: "hotelone",
+    starRating: 5,
+    facilities: ["car park", "pool"]
+  },
+  {
+    name: "hoteltwo",
+    starRating: 3,
+    facilities: ["car park", "gym"]
+  },
+  {
+    name: "hotelthree",
+    starRating: 3,
+    facilities: []
+  }
+]
+
 export const setHotels = hotels => ({
   type: "SET_HOTELS",
   hotels
 });
 
 export const startSetHotels = () => {
+
   return dispatch => {
     return axios.get("http://localhost:3001/hotels").then(res => {
-      return dispatch(setHotels(res.data));
-    });
+      return dispatch(setHotels(hotels()));
+    }).catch(error => alert(error.response));
   };
 };
